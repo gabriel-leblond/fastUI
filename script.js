6b08df30a598c8d56a425728921696420a45c3bd
Gabriel Leblond <gabriel.leblond.infographie@gmail.com>
2023-05-10 16:28:36 +0200

    New

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("conected");
+
+const mobileIconNav = document.querySelector(".mobile-nav-icon");
+const mobileModalNav = document.querySelector(".mobile-nav-modal");
+const mobileNavLikns = document.querySelectorAll(".mobile-nav-likns");
+console.info(mobileNavLikns);
+
+mobileIconNav.addEventListener("click", function () {
+  mobileIconNav.classList.toggle("fa-times");
+  mobileModalNav.classList.toggle("mobile-nav-modal-appear");
+});
+
+mobileNavLikns.forEach((i) => {
+  i.addEventListener("click", () => {
+    // console.log("Tu as clické sur le menu mobile");
+    mobileModalNav.classList.toggle("mobile-nav-modal-appear");
+  });
+});
